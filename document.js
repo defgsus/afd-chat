00ed9ac35d42493751ba3ad4cda082c9df9028da
def.gsus- <stefan.berke@modular-audio-graphics.com>
2017-06-26 16:47:10 +0200

    location hash navigation

--- document.js
+++ document.js
@@ -15,12 +15,84 @@ for (var i in chat_tokens) {
     id_to_token[chat_tokens[i][0]] = chat_tokens[i];
 }
 
+// location.-hash pseudo-navigation
+
+var oldparams = {};
+
+$(window).on("hashchange", function() {
+    let params = getNavParams();
+    console.log(params);
+    if (params.user != oldparams.user || params.token != oldparams.token)
+        showFilteredMessages();
+    if (params.user && params.user != oldparams.user) {
+        showTokensByUser(params.user);
+    }
+    if (params.msgid) {
+        scrollToMessage(params.msgid);
+    }
+    oldparams = params;
+});
+
+function getNavParams() {
+    if (!window.location.hash)
+        return {};
+    var hash = window.location.hash.substring(1).split("&");
+    var params = {};
+    for (let i in hash) {
+        let pair = hash[i].split("=");
+        if (pair && pair[0])
+            params[pair[0]] = pair[1];
+    }
+    return params;
+}
+
+function setNavParams(params) {
+    var str="";
+    if (params) {
+        str += "#";
+        for (let i in params) {
+            str += i+"="+params[i]+"&";
+        }
+    }
+    window.location.hash = str;
+}
+
+function setTokenFilter(id) {
+    var params = getNavParams();
+    if (params.token) {
+        if (!id)
+            delete params.token;
+    }
+    if (id)
+        params.token = id;
+    setNavParams(params);
+}
+
+function scrollToMessage(id) {
+    let $elem = $('.chat-message[data-msg-id="'+id+'"]');
+    if ($elem)
+        window.scrollTo({top: findElementPos($elem[0])});
+}
+
+function findElementPos(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        do {
+            curtop += obj.offsetTop;
+        } while (obj = obj.offsetParent);
+    }
+    return curtop;
+}
+
 function hookUserClick() {
     $(".user-click").on("click", function(e) {
         var user = $(e.target).attr("data-user");
         if (user) {
-            showMessagesByUser(user);
-            showTokensByUser(user);
+            var params = {user: user};
+            var msg_id = $(e.target).attr("data-msg-id");
+            if (msg_id)
+                params["msgid"] = msg_id;
+            setNavParams(params);
         }
     });
 }
@@ -31,7 +103,7 @@ function createUserList() {
         let user = user_list[i];
         $dom.append('<nobr><div class="user-entry user-click" data-user="'+user+'">' + user
                   + ' / ' + chat[user].length
-                  + (user in chat_tokens_by_user ? (' / ' + chat_tokens_by_user[user].length) : '')
+                  //+ (user in chat_tokens_by_user ? (' / ' + chat_tokens_by_user[user].length) : '')
                   + '</div></nobr>'
                    );
     }
@@ -40,10 +112,11 @@ function createUserList() {
 
 function messageDiv(msg) {
     if (typeof(msg) == "string")
-        return '<div>' + msg + '</div>';
+        return '<div class="chat-separator">' + msg + '</div>';
     var html
-         = '<div class="chat-message" >'
-         + '<span class="chat-user user-click" data-user="'+msg[2]+'">' + msg[2] + '</span> '
+         = //'<a name="msg-'+msg[0]+'">'
+           '<div class="chat-message" data-msg-id="'+msg[0]+'">'
+         + '<span class="chat-user user-click" data-user="'+msg[2]+'" data-msg-id="'+msg[0]+'">' + msg[2] + '</span> '
          + '<span class="chat-date">' + (new Date(msg[1]*1000)).toISOString() + "</span> "
          + '<div class="chat-text">' + msg[3] + '</div>'
          + '</div>';
@@ -63,16 +136,50 @@ function showMessages(messages, title) {
     hookUserClick();
 }
 
-function showMessagesByUser(user) {
+function filterMessagesByUser(messin, user) {
+    if (!user)
+        return messin;
+    var messout = [];
+    for (let i in messin) {
+        if (messin[i][2] == user)
+            messout.push(messin[i]);
+    }
+    return messout;
+}
+
+function filterMessagesByTokenId(messin, id) {
+    if (!id)
+        return messin;
+    id = parseInt(id);
+    var messout = [];
+    for (let i in messin) {
+        if (messin[i][4].includes(id))
+            messout.push(messin[i]);
+    }
+    return messout;
+}
+
+function showFilteredMessages() {
+    console.log("GILDKJHD");
+    let params = getNavParams();
+    var messages = filterMessagesByUser(chat_messages, params.user);
+    messages = filterMessagesByTokenId(messages, params.token);
+    let orig_length = messages.length;
+
+    // add some context before and after
+    let ctxrad = params.token ? 1 : 3;
     var filtmessages = {};
-    for (var i in chat_messages) {
-        i = parseInt(i);
-        for (var j=Math.max(0, i-3); j<Math.min(chat_messages.length, i+4); ++j)
-            if (chat_messages[i][2] == user)
-                if (chat_messages[j][2] == user)
-                    filtmessages[j] = chat_messages[j].concat(["highlight"]);
-                else
-                    filtmessages[j] = chat_messages[j];
+    for (var i in messages) {
+        var id = messages[i][0];
+        for (var j=Math.max(0, id-ctxrad); j<Math.min(chat_messages.length, id+ctxrad+1); ++j) {
+            j = parseInt(j);
+            if (!(j in filtmessages)) {
+                let msg = chat_messages[j];
+                filtmessages[j] = [msg[0], msg[1], msg[2], msg[3], msg[4]];
+            }
+            if (j == id)
+                filtmessages[j].push("highlight");
+        }
     }
     var messages = [];
     var last_id = 0;
@@ -82,17 +189,14 @@ function showMessagesByUser(user) {
         last_id = filtmessages[i][0];
         messages.push(filtmessages[i]);
     }
-    showMessages(messages, "messages by "+user);
+    var title = ""+orig_length+" messages";
+    if (params.user)
+        title += " by "+params.user;
+    if (params.token)
+        title += " with '"+(params.token in id_to_token ? id_to_token[params.token][1] : "UNKNOWN")+"'"
+    showMessages(messages, title);
 }
 
-function showMessagesByTokenId(id) {
-    var messages = [];
-    for (var i in chat_messages) {
-        if (chat_messages[i][4].includes(id))
-            messages.push(chat_messages[i]);
-    }
-    showMessages(messages, "messages with &quot;"+id_to_token[id][1]+"&quot;");
-}
 
 function showTokens(tokens, title) {
     var $dom = $("#token-list");
@@ -105,8 +209,9 @@ function showTokens(tokens, title) {
         $dom.append(html);
     }
     $(".token").on("click", function(e) {
-        var id = parseInt($(e.target).attr("data-id"));
-        showMessagesByTokenId(id);
+        var id = $(e.target).attr("data-id");
+        if (id)
+            setTokenFilter(parseInt(id));
     });
 }
 
@@ -126,8 +231,9 @@ function showTokensByUser(user) {
         }
     }
     $(".token").on("click", function(e) {
-        var id = parseInt($(e.target).attr("data-id"));
-        showMessagesByTokenId(id);
+        var id = $(e.target).attr("data-id");
+        if (id)
+            setTokenFilter(parseInt(id));
     });
 }
 
